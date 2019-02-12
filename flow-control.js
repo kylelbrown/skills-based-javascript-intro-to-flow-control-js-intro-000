9bba26fea13f3f7fe1f06dc908f7c5696536c3a1
Kyle Brown <kylelibrown@gmail.com>
2019-02-12 20:36:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,27 +1,27 @@
 function basicTeenager(age) {
 if (age >= 13 && age <= 19) {
-  return "You are a teenager!";
+  return "You are a teenager!"
   }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!";
+    return "You are a teenager!"
   }
   else {
-    return "You are not a teenager";
+    return "You are not a teenager"
   }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19){
-    return "You are a teenager!";
+    return "You are a teenager!"
   }
   else if (age < 13) {
-    return "You are a kid";
+    return "You are a kid"
   }
   else (age > 19) {
-    return "You are a grownup";
+    return "You are a grownup"
   }
 }
 
