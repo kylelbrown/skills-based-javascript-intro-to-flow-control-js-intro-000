1c8acf9f6385033447120b9c27414b6117b77fd4
Kyle Brown <kylelibrown@gmail.com>
2019-02-12 20:31:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,11 +15,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age >=13 && age >=19){
+  if (age >=13 && age <=19){
     return "You are a teenager!"
   }
   else if (age >13) {
-    return "You are not a kid"
+    return "You are a kid"
   }
 
 
